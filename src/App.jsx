914dd6a8e69915db40271f319cf96cbaee362669
Sveta <artist.sv.chepurna@gmail.com>
2026-05-27 19:12:36 +0200

    Refactor layout styles in App.css and index.css for improved responsiveness and alignment

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,17 @@ import './App.css'
 function App() {
 
   return (
-    
+
       <section id="center">
+        <header></header>
+
         <div>
           <h1>Get started</h1>
         </div>
+
+        <footer></footer>
       </section>
+      
 
     
     
