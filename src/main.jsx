bbc9698f761822414221e4470e86c63847a567f4
Sveta <artist.sv.chepurna@gmail.com>
2026-06-05 16:44:56 +0200

    Refactor main.jsx: implement routing with createBrowserRouter, add Elements and Error components, and maintain App rendering within StrictMode

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import Elements from './Elements.jsx'
+import Error from './Error.jsx'
+import Home from './Home.jsx'
+import EvolvingBackground from "./EvolvingBackground.jsx";
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/elements",
+    element: <Elements />
+  },
+  {
+    path: "/error",
+    element: <Error />
+  }
+]);
+
+React.DOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
+
